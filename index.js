c1d7d53eb578534a47d96b6636b0132b4221859a
khan4ubla <103826469+khan4ubla@users.noreply.github.com>
2022-06-22 22:28:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,442 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const form = document.getElementById("ans");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+
+const btn2 = document.getElementById("btn2");
+
+btn2.addEventListener("click", () => {
+  const form = document.getElementById("ans2");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn3 = document.getElementById("btn3");
+
+btn3.addEventListener("click", () => {
+  const form = document.getElementById("ans3");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn4 = document.getElementById("btn4");
+
+btn4.addEventListener("click", () => {
+  const form = document.getElementById("ans4");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn5 = document.getElementById("btn5");
+
+btn5.addEventListener("click", () => {
+  const form = document.getElementById("ans5");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn6 = document.getElementById("btn6");
+
+btn6.addEventListener("click", () => {
+  const form = document.getElementById("ans6");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn7 = document.getElementById("btn7");
+
+btn7.addEventListener("click", () => {
+  const form = document.getElementById("ans7");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn8 = document.getElementById("btn8");
+
+btn8.addEventListener("click", () => {
+  const form = document.getElementById("ans8");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn9 = document.getElementById("btn9");
+
+btn9.addEventListener("click", () => {
+  const form = document.getElementById("ans9");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn10 = document.getElementById("btn10");
+
+btn10.addEventListener("click", () => {
+  const form = document.getElementById("ans10");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn11 = document.getElementById("btn11");
+
+btn11.addEventListener("click", () => {
+  const form = document.getElementById("ans11");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn12 = document.getElementById("btn12");
+
+btn12.addEventListener("click", () => {
+  const form = document.getElementById("ans12");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn13 = document.getElementById("btn13");
+
+btn13.addEventListener("click", () => {
+  const form = document.getElementById("ans13");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn14 = document.getElementById("btn14");
+
+btn14.addEventListener("click", () => {
+  const form = document.getElementById("ans14");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn15 = document.getElementById("btn15");
+
+btn15.addEventListener("click", () => {
+  const form = document.getElementById("ans15");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn16 = document.getElementById("btn16");
+
+btn16.addEventListener("click", () => {
+  const form = document.getElementById("ans16");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+
+const btn17 = document.getElementById("btn17");
+
+btn17.addEventListener("click", () => {
+  const form = document.getElementById("ans17");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn18 = document.getElementById("btn18");
+
+btn18.addEventListener("click", () => {
+  const form = document.getElementById("ans18");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn19 = document.getElementById("btn19");
+
+btn19.addEventListener("click", () => {
+  const form = document.getElementById("ans19");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn20 = document.getElementById("btn20");
+
+btn20.addEventListener("click", () => {
+  const form = document.getElementById("ans20");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn21 = document.getElementById("btn21");
+
+btn21.addEventListener("click", () => {
+  const form = document.getElementById("ans21");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn22 = document.getElementById("btn22");
+
+btn22.addEventListener("click", () => {
+  const form = document.getElementById("ans22");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn23 = document.getElementById("btn23");
+
+btn23.addEventListener("click", () => {
+  const form = document.getElementById("ans23");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn24 = document.getElementById("btn24");
+
+btn24.addEventListener("click", () => {
+  const form = document.getElementById("ans24");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn25 = document.getElementById("btn25");
+
+btn25.addEventListener("click", () => {
+  const form = document.getElementById("ans25");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn26 = document.getElementById("btn26");
+
+btn26.addEventListener("click", () => {
+  const form = document.getElementById("ans26");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn27 = document.getElementById("btn27");
+
+btn27.addEventListener("click", () => {
+  const form = document.getElementById("ans27");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn28 = document.getElementById("btn28");
+
+btn28.addEventListener("click", () => {
+  const form = document.getElementById("ans28");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn29 = document.getElementById("btn29");
+
+btn29.addEventListener("click", () => {
+  const form = document.getElementById("ans29");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn30 = document.getElementById("btn30");
+
+btn30.addEventListener("click", () => {
+  const form = document.getElementById("ans30");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn31 = document.getElementById("btn31");
+
+btn31.addEventListener("click", () => {
+  const form = document.getElementById("ans31");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn32 = document.getElementById("btn32");
+
+btn32.addEventListener("click", () => {
+  const form = document.getElementById("ans32");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn33 = document.getElementById("btn33");
+
+btn33.addEventListener("click", () => {
+  const form = document.getElementById("ans33");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn34 = document.getElementById("btn34");
+
+btn34.addEventListener("click", () => {
+  const form = document.getElementById("ans34");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn35 = document.getElementById("btn35");
+
+btn35.addEventListener("click", () => {
+  const form = document.getElementById("ans35");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn36 = document.getElementById("btn36");
+
+btn36.addEventListener("click", () => {
+  const form = document.getElementById("ans36");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn37 = document.getElementById("btn37");
+
+btn37.addEventListener("click", () => {
+  const form = document.getElementById("ans37");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn38 = document.getElementById("btn38");
+
+btn38.addEventListener("click", () => {
+  const form = document.getElementById("ans38");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn39 = document.getElementById("btn39");
+
+btn39.addEventListener("click", () => {
+  const form = document.getElementById("ans39");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
+const btn40 = document.getElementById("btn40");
+
+btn40.addEventListener("click", () => {
+  const form = document.getElementById("ans40");
+
+  if (form.style.visibility === "hidden") {
+    form.style.visibility = "visible";
+  } else {
+    form.style.visibility = "hidden";
+  }
+});
